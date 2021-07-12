5747cc3ceedd9c70c455ac07feef06f33ba21dfb
iudini <hoiitzx@gmail.com>
2021-07-12 15:14:15 +0300

    4. Регистрация пользователя. [#283110]

--- src/main/java/ru/job4j/dream/servlet/RegServlet.java
+++ src/main/java/ru/job4j/dream/servlet/RegServlet.java
@@ -2,7 +2,6 @@ package ru.job4j.dream.servlet;
 
 import ru.job4j.dream.model.User;
 import ru.job4j.dream.store.PsqlStore;
-import ru.job4j.dream.store.Store;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -21,6 +20,10 @@ public class RegServlet extends HttpServlet {
         String name = req.getParameter("name");
         String email = req.getParameter("email");
         String password = req.getParameter("password");
+        if (PsqlStore.instOf().findByEmail(email) != null) {
+            req.setAttribute("error", "пользователь с данным email уже зарегистрирован");
+            req.getRequestDispatcher("reg.jsp").forward(req, resp);
+        }
         PsqlStore.instOf().save(new User(0, name, email, password));
         resp.sendRedirect(req.getContextPath() + "/login.jsp");
     }
